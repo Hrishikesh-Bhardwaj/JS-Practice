cba7f4a6ce0ac2e81799dd04137d2485897ec7a5
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-05 07:26:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+/*
+    number, bigint, string, boolean(true/false), null(representation of empty var :standalone value), undefined(value not define)
+    symbol: mostly used for uniqueness
+    object: 
+
+*/
+
+console.log(typeof "Hrishikesh") //string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
