4ff20af865e6ea803373b736f6ad1e54bffd8fd4
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-05 16:31:02 +0000

    updated

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -14,3 +14,9 @@
 // String--> String
  
 
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+// JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
