d8f1805326562f9f08e50549cede0c3253305050
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-05 18:22:57 +0000

    numMath

---
+++ 01_basics/05_numAndMaths.js
@@ -0,0 +1,18 @@
+//##################    Maths   #################3
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2, 1, 5, 6));
+// console.log(Math.max(2, 1, 5, 6));
+
+console.log(Math.random()) // value b/w 0 and 1
+console.log(Math.random()*10 + 1);
+
+
+const min = 10; 
+const max = 20;
+
+Math.random()
