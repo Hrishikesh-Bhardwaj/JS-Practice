793ab5d7249fcec56e4874c5390ff6dab00fa959
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-07 05:11:04 +0000

    object destructing

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+const course = {
+    courseName : "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+const {courseInstructor: cIns} = course // destructuring object
+console.log(cIns) 
+
+
+
