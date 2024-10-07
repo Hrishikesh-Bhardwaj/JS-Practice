b39db796146aaf2668648412cf043362e2135319
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-07 12:34:17 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,17 @@
+// function chai(){
+//     let username = "hrishikesh"
+//     console.log(this.username);    
+// }
+
+// const chai = ()=>{
+//     let username = "hrishikesh"
+//     console.log(this);    
+// }
+
+// chai()
+
+const add = (num1, num2)=>{
+    return num1+num2
+}
+
+console.log(add(3, 8));
