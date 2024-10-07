681ae7ed7fd2a92d81d63e51a6ddf0e2105f7874
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-07 14:53:54 +0000

    arrowUp

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -15,3 +15,7 @@ const add = (num1, num2)=>{
 }
 
 console.log(add(3, 8));
+
+const addNum = (num1, num2)=> num1+num2; // implicit return. to return object wrap it into prethesis().
+
+console.log(addNum(2, 3))
