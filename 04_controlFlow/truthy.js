0eda60b5c8f680eff895d4350d318d2fdd31efe9
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-08 08:14:28 +0000

    truthy NUllish

--- 04_controlFlow/truthy.js
+++ 04_controlFlow/truthy.js
@@ -6,6 +6,15 @@ truthy values:  "0", 'false', " ", [], {}, function(){}
 const emptyObject = {}
 
 if(Object.keys(emptyObject).length===0){
-    console.log("empty object");
+    // console.log("empty object");
     
 }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+val1 = null ?? 10
+console.log(val1);
+
