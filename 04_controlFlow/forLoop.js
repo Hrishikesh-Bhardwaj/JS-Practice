153898265bc1a1b14b5a79aaa058f1fdd1befb29
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-08 11:39:38 +0000

    forLoop

---
+++ 04_controlFlow/forLoop.js
@@ -0,0 +1,12 @@
+ for (let index = 0; index <= 10; index++) {
+    const element = index;
+   //  console.log(element);   
+ }
+ 
+
+ for(let i = 0; i <=10; i++){
+      if(i==5){
+         console.log("5 is best number");         
+      }
+      console.log(i);
+ }
