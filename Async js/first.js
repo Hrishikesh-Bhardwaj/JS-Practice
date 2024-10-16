7d7d39c2bd9b25c0a906605e2385406e93b2b488
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-16 06:43:12 +0000

    Async js

---
+++ Async js/first.js
@@ -0,0 +1,21 @@
+/*
+
+    JavaScript:  
+            => Synchronous (each code execute at a time) (default)
+            => Single Threaded () (default)
+
+    Execution Context: 
+            => execute one line of code at a time (each operation waits untill last operation gets executed)
+            => console log 1 
+            => console log 2 
+
+            -----------------                 ---------------
+            |  call stack   |                 |  memory heap |
+            |               |                 |              |
+            ----------------                  ----------------  
+    
+     ## Blocking Code v/s non Blocking code 
+        blocking code        : block the flow of code 
+        non blocking code    : does not block flow of code        
+          
+*/
