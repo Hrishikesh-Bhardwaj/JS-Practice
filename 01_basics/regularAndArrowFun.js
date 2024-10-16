e65bc79f259a41a39c8cafa5ec9bb2497193e858
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-16 06:26:16 +0000

    diff regular fun and arrow func

--- 01_basics/regularAndArrowFun.js
+++ 01_basics/regularAndArrowFun.js
@@ -7,6 +7,8 @@
     4. implicitly returned by the arrow function 
 */
 
+//### argument object are not available in arrow functions,#####
+
 /*
 function add(x, y){
     console.log(arguments)
@@ -17,8 +19,72 @@ add(10, 15)
 */
 
 const add = (x, y)=>{
-    console.log(arguments);
+    // console.log(arguments);
     return x+y;
 }
 
 add(10, 15)
+
+// arrow functions do not have their own this.
+
+// function car(name){
+//     this.brand = name
+// }
+
+// let carData = new car('maruti')
+// console.log(carData)
+
+// const car = (name)=>{
+//     this.brand = name;
+// }
+// let carData2 = new car('maruti');
+
+const obj1 ={
+    name: 'hrishikesh',
+    printName(){
+        const test = ()=>{
+            // console.log(this.name+ ' arrow inside normal function')
+        }
+        test();
+        // console.log(this.name+ ' normal function')
+    },
+    printName2 : ()=>{
+        // console.log(this.name+ ' arrow function')
+    }
+    
+}
+
+obj1.printName()
+obj1.printName2()
+
+// implicitly returned by the arrow function 
+
+function marks(){
+    100;
+}
+console.log(marks()) // undefined
+
+const marks2 = ()=>100;
+console.log(marks2()) // 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
