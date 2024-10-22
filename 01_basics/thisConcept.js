9ab3bbb993337abbedf18e151504b3557777af8a
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-10-22 01:34:49 +0000

    thisConcept2

--- 01_basics/thisConcept.js
+++ 01_basics/thisConcept.js
@@ -16,14 +16,15 @@
             console.log(this) // obj.x() output is : object
         }
     }
-
+    call(), apply(), bind() => All these use to set value of this.
 */
+// 1. call()
 let userDetail = {
     name: 'hrishikesh',
     age: 29, 
     designation: "salesforce developer",
     printDetail: function(){
-        console.log(this.name);
+        // console.log(this.name);
     }
 }
 
@@ -38,4 +39,15 @@ let userDetail2 = {
 userDetail.printDetail.call(userDetail2); // this is called function borrowing using call() method.
 
 // if function is global
+function printDetail(){
+    // console.log(this.name);
+}
 
+// printDetail.call(userDetail); // hrishikesh
+// printDetail.call(userDetail2); // bhardwaj
+
+
+// 2. apply() : multiples parameter passed as an array list by using apply().
+
+printDetail.apply(userDetail2, ["delhi", "India"]);
+console.log(userDetail2)
